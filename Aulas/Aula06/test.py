675897500c3248d46f009eb46400a82cd1d539da
CAFÉ <pciskolargx@gmail.com>
2025-11-22 10:56:12 -0300

    entendi

--- Aulas/Aula06/test.py
+++ Aulas/Aula06/test.py
@@ -1,11 +1,18 @@
-opcao = input("Escolha uma opção (1,2 ou 3): ")
 
-match opcao: 
-    case "1":
-        print("Você escolheu a opção 1")
-    case "2":
-        print("Você escolheu a opção 2")
-    case "3":
-        print("Você escolheu a opção 3")
-    case _:
-        print("Opção inválida")
+bangu = int(input("Quantos Gols do Bangu?"))
+madureira = int(input("Quantos Gols do Madureira?"))
+
+diferenca = bangu - madureira
+
+if diferenca > 4:
+    print(f"DIFERENÇA:  {diferenca}")
+    print("STATUS:  GOLEADA")
+elif diferenca < -4:
+    print(f"DIFERENÇA:  {abs(diferenca)}")
+    print("STATUS:  GOLEADA{cores['limpa']}")
+elif diferenca == 0:
+    print(f"DIFERENÇA: {diferenca}")
+    print("STATUS: EMPATE!")
+else:
+    print(f"DIFERENÇA:  {abs(diferenca)}")
+    print(f"STATUS: PARTIDA NORMAL")
