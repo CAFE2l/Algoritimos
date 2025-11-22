6cc15ceb954830d9e3c077ea315a7982268fc37c
CAFÉ <pciskolargx@gmail.com>
2025-11-22 11:38:39 -0300

    somatorio dos números finalizado

--- Aulas/Aula07/test.py
+++ Aulas/Aula07/test.py
@@ -1,7 +1,15 @@
 cont = 1
 soma = 0
+maior = 0
+menor = 0 
 while cont <= 10: 
     num = int(input(f"Digite o {cont}º número: "))
+    if num > maior:
+        maior = num
+    if num < menor: 
+        menor = num       
     soma += num
     cont += 1
+print(f"O maior número é {maior}")
+print(f"O menor número é {menor}")
 print("A soma dos números é:", soma)
