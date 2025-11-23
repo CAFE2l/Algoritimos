5aef52bd1af428ecaa379602bc5c5bc10608fdba
CAFÉ <pciskolargx@gmail.com>
2025-11-23 13:07:38 -0300

    ARREGUEI PAREI NO MINUTO 36:51

--- Aulas/Aula12/OrdernarVetor.py
+++ Aulas/Aula12/OrdernarVetor.py
@@ -1,17 +1,20 @@
 vet = []          # Cria lista vazia para armazenar os valores
 aux = 0
-for i in range(0, 4):
+for i in range(0, 10):
     valor = int(input("Digite um valor: "))   # Lê valor do usuário
     vet.append(valor)                         # Adiciona valor à lista
 
 # Algoritmo de ordenação simples (bubble sort)
-for i in range(0, 3):
-    for j in range(i+1, 4):
+for i in range(0, 9):
+    for j in range(i+1, 10):
         if vet[i] > vet[j]:                  # Se valor da posição i for maior, troca
             aux = vet[i]
             vet[i] = vet[j]
             vet[j] = aux
 
 # Mostra os valores ordenados
-for i in range(0, 4):
+for i in range(0, 10):
     print(vet[i], end=" ")
+
+
+#ARREGUEI PAREI NO MINUTO 36:51 
