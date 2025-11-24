b4ac92f004488e7440a25d8a4a2cb7d2cda15ed3
CAFÉ <pciskolargx@gmail.com>
2025-11-24 16:48:56 -0300

    teste de matriz 3x3

---
+++ Aulas/Aula13/matriz3x3.py
@@ -0,0 +1,38 @@
+# Cria uma matriz 3x2 (3 linhas, 2 colunas) inicializada vazia
+matriz = []
+numerosPares = []
+numerosImpares = []
+# Preenche a matriz com valores digitados pelo usuário
+for L in range(0, 3):
+    linha = []  # Cria uma nova linha vazia
+    for c in range(0, 3):
+        valor = int(input(f"Digite o valor da posição [{L},{c}]: ")) # Lê o valor
+        if valor % 2 == 0:
+            numerosPares.append(valor)
+        else: 
+            numerosImpares.append(valor)
+        linha.append(valor)  # Adiciona o valor na linha
+    matriz.append(linha)     # Adiciona a linha completa na matriz
+
+# Exibe os valores da matriz formatados
+for L in range(0, 3):
+    for c in range(0, 2):
+        print(f"{matriz[L][c]:4}", end=" ")  # Mostra cada elemento da linha
+    print()  # Pula para a próxima linha
+
+print(f"Os valores pares foram: {numerosPares}")
+print(f"Os valores impares foram: {numerosImpares}")
+
+#Solução do guaná:
+
+matriz = []
+
+for L in range(0, 3):
+    for c in range(0, 3):
+        valor = int(input(f"Digite o valor da posição [{L},{c}]: "))
+        matriz.append(valor)
+
+for L in range(0, 3):
+    for c in range(0, 3):
+        print(f"{matriz[L, c]:4}", end=" ")
+    print()
