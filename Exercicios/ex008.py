2958543d08617b81d5ddf23f50b065fd7b3676db
CAFÉ <pciskolargx@gmail.com>
2025-11-26 06:46:13 -0300

    ex 08, 09 e 10 criados

---
+++ Exercicios/ex008.py
@@ -0,0 +1,14 @@
+metros = float(input(f"Digite uma distância em metros: "))
+print(f"A distância de {metros}m corresponde a: ")
+km = metros / 1000
+hm = metros / 100
+dam = metros / 10
+dm = metros * 10
+cm = metros * 100
+mm = metros * 1000
+print(f"{km}km")
+print(f"{hm}hm")
+print(f"{dam}dam")
+print(f"{dm}dm")
+print(f"{cm}cm")
+print(f"{mm}mm")
---
+++ Exercicios/ex009.py
@@ -0,0 +1,3 @@
+money = float(input(f"Quanto dinheiro vc tem na carteira: "))
+dolar = money / 5.20
+print(f"com R${money} você pode comprar US${dolar:.2f}")
