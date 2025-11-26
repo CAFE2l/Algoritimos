7423e8547f771fd4a84cfcaa8627d1ee0a169a35
CAFÉ <pciskolargx@gmail.com>
2025-11-26 06:48:06 -0300

    ex010 finalizado

---
+++ Exercicios/ex010.py
@@ -0,0 +1,7 @@
+altura = float(input("Qual a altura da parede? "))
+largura = float(input("Qual a largura da parede? "))
+
+area = altura * largura
+tinta = area / 2
+
+print(f"para pintar a parede será necessário {tinta} litros de tinta")
