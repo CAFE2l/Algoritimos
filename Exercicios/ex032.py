8efd6538bcf1731e0e021f08398f31e42c15274d
CAFÉ <pciskolargx@gmail.com>
2025-11-26 10:31:55 -0300

    ex032 finalizado

---
+++ Exercicios/ex032.py
@@ -0,0 +1,11 @@
+import random 
+
+while True:
+    num = int(input("tente adivinhar um número entre 1 e 5: "))
+    computador = random.randint(1, 5)
+
+    if (num == computador):
+        print("Parabéns vc acertou!!!")
+        break
+    else:
+        print("ERROU, tente novamente")
