fdd5b8101d61bb315e02491985564c1f2b3d2a92
CAFÉ <pciskolargx@gmail.com>
2025-11-26 06:59:25 -0300

    ex013 finalizado

---
+++ Exercicios/ex013.py
@@ -0,0 +1,4 @@
+salario = 5000
+aumento = salario * 0.15
+novoSalario = salario + aumento
+print(f"O funcionário que tinha o salário {salario} agora com 15% de aumento passa a ganhar {novoSalario:.2f}")
