5daae439b59f555cef09b019b20fa41b627cc56a
CAFÉ <pciskolargx@gmail.com>
2025-11-26 06:52:56 -0300

    ex011 finalizado

---
+++ Exercicios/ex011.py
@@ -0,0 +1,5 @@
+A = int(input(f"Digite o valor de A: "))
+B = int(input(f"Digite o valor de B: "))
+C = int(input(f"Digite o valor de C: "))
+Delta = B**2 -4*A*C
+print(f"O valor de Delta é {Delta}")
