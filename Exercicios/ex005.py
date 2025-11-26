503730130fae8617781a7df1013a326f18173db3
CAFÉ <pciskolargx@gmail.com>
2025-11-26 06:34:54 -0300

    ex005 finalizado

---
+++ Exercicios/ex005.py
@@ -0,0 +1,4 @@
+Nota1 = float(input(f"Digite sua primeira nota: "))
+Nota2 = float(input(f"Digite sua segunda nota: "))
+Média = (Nota1 + Nota2) / 2
+print(f"A média entre {Nota1} e {Nota2} é igual a {Média}")
