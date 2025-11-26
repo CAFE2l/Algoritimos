52d3440e6cc8448e7ef5b8b8d5b620369936689f
CAFÉ <pciskolargx@gmail.com>
2025-11-26 07:21:39 -0300

    ex015 finalizado

---
+++ Exercicios/ex015.py
@@ -0,0 +1,5 @@
+horas = 8
+ReaisporHora = 25
+Dias = int(input(f"Quantos dias você trabalhou no mês? "))
+salario = Dias * (ReaisporHora * horas)
+print(f"o salário do funcionário é de R${salario:.2f}")
