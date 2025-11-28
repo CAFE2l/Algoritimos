45982b5f2fc0d54d162d9bd3c95ab545452c7f68
CAFÉ <pciskolargx@gmail.com>
2025-11-28 15:34:26 -0300

    ex088 finalizado

---
+++ Exercicios/ex088.py
@@ -0,0 +1,9 @@
+frase = input("Digite uma frase qualquer: ")
+
+def msg():
+    print("+-----==============-----+")
+    for i in range(0, 4):
+        print(frase)
+    print("+-----==============-----+")
+
+msg()
