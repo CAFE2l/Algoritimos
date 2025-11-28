742487ec203745fca062775996869481bf507b3c
CAFÉ <pciskolargx@gmail.com>
2025-11-28 15:43:26 -0300

    ex090 finalizado

---
+++ Exercicios/ex090.py
@@ -0,0 +1,9 @@
+def somador():
+    num1 = int(input("Digite o primeiro valor: "))
+    num2 = int(input("Digite o segundo valor: "))
+    soma = num1 + num2
+    return print(soma)
+
+
+somador()
+
